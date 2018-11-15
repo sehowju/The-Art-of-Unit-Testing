cd46857e98fd33e70d2741dd3c83634f15d8879b
DinoLai <dinos80152@gmail.com>
2018-11-15 16:42:00 +0800

    fix typo

--- ch5/Tests/LogAnalyzer3Test.php
+++ ch5/Tests/LogAnalyzer3Test.php
@@ -8,20 +8,8 @@ use Ch5\Src\LoggerInterface;
 use Ch5\Src\WebServiceInterface;
 use Ch5\Src\ErrorInfo;
 
-class LogAnalyzer2Test extends TestCase
+class LogAnalyzer3Test extends TestCase
 {
-    // public function test_Analyze_loggerThrows_CallsWebService()
-    // {
-    //     $mockService = $this->createMock(WebServiceInterface::class);
-    //     $stubLogger = $this->createMock(LoggerInterface::class);
-    //     $stubLogger->method("logError")->with($this->isType("string"))->will($this->throwException(new \Exception("fake exception")));
-    //     $mockService->expects($this->once())->method("write")->with($this->stringContains("fake exception"));
-
-    //     $analyzer = new LogAnalyzer2($stubLogger, $mockService);
-    //     $analyzer->minNameLength = 10;
-    //     $analyzer->analyze("Short.txt");
-    // }
-
     public function test_Analyze_loggerThrows_CallsWebServiceWithSubObject()
     {
         $mockService = $this->createMock(WebServiceInterface::class);
@@ -29,7 +17,7 @@ class LogAnalyzer2Test extends TestCase
         $stubLogger->method("logError")->with($this->isType("string"))->will($this->throwException(new \Exception("fake exception")));
         $mockService->expects($this->once())->method("write")->with(new ErrorInfo(1000, "fake exception"));
 
-        $analyzer = new LogAnalyzer2($stubLogger, $mockService);
+        $analyzer = new LogAnalyzer3($stubLogger, $mockService);
         $analyzer->minNameLength = 10;
         $analyzer->analyze("Short.txt");
     }
